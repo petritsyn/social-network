955112b6ea64dafcd7bb7e8e6524be84a4d7ded3
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-03-18 18:21:17 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+    <img src="https://image.shutterstock.com/image-vector/vector-design-elements-your-company-260nw-709133980.jpg"/>
+  </header>
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return   <div className='content'>
+    <div>
+      <img src="https://www.w3schools.com/howto/img_snow_wide.jpg"/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      New Post
+    </div>
+    <div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+};
+
+export default Profile;
