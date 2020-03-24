9f68dc8035889f02cca60b99414a89cab07191d6
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-03-24 20:57:10 +0300

    Begin to stydy props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div className={classes.posts}>
-        <Post />
-        <Post />
+        <Post message='Hi, how are you?' />
+        <Post message ='Its my first post.'/>
       </div>
     </div>
 )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+  console.log(props.message);
   return (
-        <div className={classes.item}>
+  <div className={classes.item}>
           <img src="https://avatars.mds.yandex.net/get-pdb/1979423/4f56fe36-7a8d-4805-bde3-039bd14a6b7f/s1200?webp=false"/>
-          post 1
+    {props.message};
           <div>
             <span>Like</span>
           </div>
