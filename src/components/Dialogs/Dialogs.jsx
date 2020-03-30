7529a4cc6ed1ed2a0bf95e4cfefee38115aa30e1
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-03-30 19:40:02 +0300

    Lesson 28. Relocate data to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,26 +5,8 @@ import Message from './../Dialogs/Message/Message';
 
 const Dialogs = (props) => {
 
-  let dialogs = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Andrew'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'},
-    {id: 5, name: 'Victor'},
-    {id: 6, name: 'Valera'},
-  ];
-
-  let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'Hello'},
-    {id: 3, message: 'How are you?'},
-    {id: 4, message: 'Yo'},
-    {id: 5, message: 'Yo'},
-    {id: 6, message: 'Yo'},
-  ];
-
-  let dialogsElements = dialogs.map(dialog => <DialogsItem name={dialog.name} id={dialog.id}/>);
-  let messegesElements = messages.map(m => <Message message={m.message}/>);
+  let dialogsElements = props.dialogs.map(dialog => <DialogsItem name={dialog.name} id={dialog.id}/>);
+  let messegesElements = props.messages.map(m => <Message message={m.message}/>);
 
   return (
     <div className={s.dialogs}>
