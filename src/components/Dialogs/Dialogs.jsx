dd32805d17fcee803e6960c7fd0a9fa0738b8e4d
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-03-30 14:51:11 +0300

    Lesson 26

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
-
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-
-  return (
-    <div className={s.dialog + ' ' + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  )
-}
-
-const Message = (props) => {
-  return <div className={s.message}>{props.message}</div>
-}
+import DialogsItem from './DialogItem/DialogsItem';
+import Message from './../Dialogs/Message/Message';
 
 const Dialogs = (props) => {
 
@@ -37,7 +23,7 @@ const Dialogs = (props) => {
     {id: 6, message: 'Yo'},
   ];
 
-  let dialogsElements = dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+  let dialogsElements = dialogs.map(dialog => <DialogsItem name={dialog.name} id={dialog.id}/>);
   let messegesElements = messages.map(m => <Message message={m.message}/>);
 
   return (
