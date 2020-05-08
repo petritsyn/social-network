bb8bddf3c8165abbb295542d160e8b958e32ba15
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-05-08 19:26:34 +0300

    Lesson 31. CreateRef

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,13 @@ const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map(dialog => <DialogsItem name={dialog.name} id={dialog.id}/>);
   let messegesElements = props.state.messages.map(m => <Message message={m.message}/>);
 
+  let newMessageElement = React.createRef();
+
+  let addMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text)
+  }
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
@@ -15,6 +22,12 @@ const Dialogs = (props) => {
       </div>
       <div className={s.messages}>
         {messegesElements}
+        <div>
+          <textarea ref={newMessageElement} />
+          <div>
+            <button onClick={addMessage}>Send message</button>
+          </div>
+        </div>
       </div>
     </div>
   )
