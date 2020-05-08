46ffc56b20101d02ca84c08e6c76abe7c9fae1ab
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-05-08 20:27:25 +0300

    Lesson 32. Callback through props

--- src/App.js
+++ src/App.js
@@ -7,24 +7,23 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
+
 
 const App = (props) => {
 
   return (
-    <BrowserRouter>
       <div className='app-wrapper'>
         <Header/>
         <Navbar/>
         <div className='app-wrapper-content'>
-          <Route path='/profile' render={ () => <Profile state={props.state.profilePage} /> } />
+          <Route path='/profile' render={ () => <Profile state={props.state.profilePage} addPost={props.addPost} /> } />
           <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
           <Route path='/news' render={ () => <News />} />
           <Route path='/music' render={ () => <Music />} />
           <Route path='/settings' render={ () => <Settings />} />
         </div>
       </div>
-    </BrowserRouter>
   );
 }
 
