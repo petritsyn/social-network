87f6e4e572fa17e9750f9e743998274e17536dfd
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-05-08 19:09:43 +0300

    1

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,8 +5,10 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
+
   let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
+  
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
