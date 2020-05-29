57d9d4b5138a7e55e1da1cba60465af12145b586
youngvvp <andrey2343@yandex.ru>
2020-05-29 18:05:30 +0300

    61 - cookie

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-  return <header className={classes.header}>
-    <img src="https://image.shutterstock.com/image-vector/vector-design-elements-your-company-260nw-709133980.jpg"/>
-  </header>
+
+const Header = (props) => {
+    return <header className={classes.header}>
+        <img src="https://image.shutterstock.com/image-vector/vector-design-elements-your-company-260nw-709133980.jpg"/>
+        <div className={classes.loginBlock}>
+         {props.isAuth ? props.login :  <NavLink to={'/login'}>Login</NavLink>}
+        </div>
+    </header>
 };
+
 
 export default Header;
