4742608571eab032a4d24748830a0fcf5bf42a0a
youngvvp <andrey2343@yandex.ru>
2020-06-03 18:10:58 +0300

    63 - DAL

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import * as axios from "axios";
+
+
+let instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '7d54e03a-c727-4a11-92e7-335f41a4e836'
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    },
+
+    getUsers2(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+};
