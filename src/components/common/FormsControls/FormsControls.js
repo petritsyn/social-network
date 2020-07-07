0e555f81a5bbb0aaf0af19ac4b0351c3359ca917
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-07-08 00:35:09 +0300

    77 - redux-form field validation

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -2,26 +2,22 @@ import React from "react";
 import styles from './FormsControls.module.css';
 
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, child, ...props}) => {
     const hasError = meta.error && meta.touched;
     return <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
         <div>
-            <textarea {...input} {...props}/>
+            {props.children}
         </div>
-        <div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
+        {hasError && <span>{meta.error}</span>}
     </div>
 };
 
-export const Input = ({input, meta, ...props}) => {
-    const hasError = meta.error && meta.touched;
-    return <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
-        <div>
-            <input {...input} {...props}/>
-        </div>
-        <div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    </div>
+export const Textarea = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+};
+
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
 };
