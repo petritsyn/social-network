f0e6d8e85a78f66ccca2d3ae38a2a042834f07c2
youngvvp <53496390+youngvvp@users.noreply.github.com>
2020-08-12 16:34:09 +0300

    95 - github pages

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import store from './redux/redux-store';
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 
